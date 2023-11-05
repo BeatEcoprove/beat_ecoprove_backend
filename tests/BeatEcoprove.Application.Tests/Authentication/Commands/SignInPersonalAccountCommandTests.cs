1bf51e1ba6fb8b523ec2fbdcf2198d96122d1055
Diogo Assunção <dassuncao1234@gmail.com>
2023-11-05 00:10:13 +0000

    add: Tests to Application layer

---
+++ tests/BeatEcoprove.Application.Tests/Authentication/Commands/SignInPersonalAccountCommandTests.cs
@@ -0,0 +1,110 @@
+﻿
+using BeatEcoprove.Application.Authentication.Commands.SignInPersonalAccount;
+using BeatEcoprove.Application.Shared.Interfaces.Persistence;
+using BeatEcoprove.Application.Shared.Interfaces.Providers;
+using BeatEcoprove.Domain.ProfileAggregator;
+using BeatEcoprove.Domain.ProfileAggregator.ValueObjects;
+using BeatEcoprove.Domain.Shared.Errors;
+using Bogus;
+using NSubstitute;
+
+namespace BeatEcoprove.Application.Tests.Authentication.Commands;
+
+public class SignInPersonalAccountCommandTests
+{
+    private readonly IAuthRepository _authRepository = Substitute.For<IAuthRepository>();
+    private readonly IProfileRepository _profileRepository = Substitute.For<IProfileRepository>();
+    private readonly IUnitOfWork _unitOfWork = Substitute.For<IUnitOfWork>();
+    private readonly IJwtProvider _jwtProvider = Substitute.For<IJwtProvider>();
+    private readonly IPasswordProvider _passwordProvider = Substitute.For<IPasswordProvider>();
+
+    private readonly SignInPersonalAccountCommandHandler _sut;
+
+    public SignInPersonalAccountCommandTests()
+    {
+        _sut = new SignInPersonalAccountCommandHandler(
+            _authRepository,
+            _profileRepository,
+            _unitOfWork,
+            _jwtProvider,
+            _passwordProvider);
+    }
+
+    private async Task<Stream> GetAvatarPicture()
+    {
+        using HttpClient httpClient = new();
+        var response = httpClient.GetAsync("https://github.com/DiogoCC7.png");
+
+        using Stream stream = await response.Result.Content.ReadAsStreamAsync();
+        return stream;
+    }
+
+    private async Task<SignInPersonalAccountCommand> GetSutCommand()
+    {
+        var avatarPicture = await GetAvatarPicture();
+
+        // Arrange
+        return new Faker<SignInPersonalAccountCommand>()
+            .CustomInstantiator(f => new SignInPersonalAccountCommand(
+                f.Person.FullName,
+                DateOnly.FromDateTime(f.Person.DateOfBirth),
+                f.Internet.UserName(),
+                "Male",
+                "+351",
+                "914953124",
+                avatarPicture,
+                f.Internet.Email(),
+                "Password12"))
+            .Generate();
+    }
+
+    // TODO: Validate if user exists should return error
+    [Fact]
+    public async Task ShouldNot_SignInPersonalAccount_WhenUserAlreadyExists()
+    {
+        // Arrange
+        var command = await GetSutCommand();
+
+        _authRepository.ExistsUserByEmailAsync(
+            Email.Create(command.Email).Value,
+            default).Returns(true);
+
+        // Act
+        var result = await _sut.Handle(command, default);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.Equal(result.FirstError.Code, Errors.User.EmailAlreadyExists.Code);
+    }
+
+    [Fact]
+    public async Task ShouldNot_SignInPersonalAccount_WhenUserNameAlreadyUsed()
+    {
+        // Arrange
+        var command = await GetSutCommand();
+
+        _profileRepository.ExistsUserByUserNameAsync(
+            UserName.Create(command.UserName),
+            default).Returns(true);
+
+        // Act
+        var result = await _sut.Handle(command, default);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.Equal(result.FirstError.Code, Errors.User.UserNameAlreadyExists.Code);
+    }
+
+    [Fact]
+    public async Task Should_ReturnAuthTokens_WhenUserSignInPersonalAccount()
+    {
+        var command = await GetSutCommand();
+
+        // Act
+        var result = await _sut.Handle(command, default);
+
+        // Assert
+        Assert.False(result.IsError);
+        await _authRepository.Received(1).AddAsync(Arg.Any<Auth>(), default);
+    }
+}
---
+++ tests/BeatEcoprove.Application.Tests/Authentication/Queries/LoginQueryTests.cs
@@ -0,0 +1,164 @@
+﻿using BeatEcoprove.Application.Authentication.Queries.Login;
+using BeatEcoprove.Application.Shared.Interfaces.Providers;
+using BeatEcoprove.Domain.ProfileAggregator;
+using BeatEcoprove.Domain.ProfileAggregator.Entities;
+using BeatEcoprove.Domain.ProfileAggregator.ValueObjects;
+using BeatEcoprove.Domain.Shared.Errors;
+using Bogus;
+using NSubstitute;
+
+namespace BeatEcoprove.Application.Tests;
+
+public class LoginQueryTests
+{
+    private readonly IAuthRepository _authRepository = Substitute.For<IAuthRepository>();
+    private readonly IProfileRepository _profileRepository = Substitute.For<IProfileRepository>();
+    private readonly IJwtProvider _jwtProvider = Substitute.For<IJwtProvider>();
+    private readonly IPasswordProvider _passwordProvider = Substitute.For<IPasswordProvider>();
+
+    private readonly LoginQueryHandler _sut;
+
+    public LoginQueryTests()
+    {
+        _sut = new LoginQueryHandler(
+            _authRepository,
+            _profileRepository,
+            _jwtProvider,
+            _passwordProvider
+        );
+    }
+
+    private LoginQuery GetSutQuery()
+    {
+        // Arrange
+        return new Faker<LoginQuery>()
+            .CustomInstantiator(f => new LoginQuery(
+                f.Internet.Email(),
+                "Password12"))
+            .Generate();
+    }
+
+    private Auth GetAuth(string fakeEmail)
+    {
+        Email email = Email.Create(fakeEmail).Value;
+        var password = Password.Create("Password12").Value;
+
+        return Auth.Create(email, password);
+    }
+
+    private Organization GetProfile(AuthId authId)
+    {
+        var address = Address.Create(
+              "Rua da Liberdade",
+              12,
+              "Porto",
+              PostalCode.Create("4000-001").Value
+              ).Value;
+
+        return new Faker<Organization>()
+            .CustomInstantiator(f => Organization.Create(
+                authId,
+                UserName.Create(f.Person.UserName),
+                Phone.Create("+351", "915682123").Value,
+                "https://github.com/DiogoCC7.png",
+                address
+                ))
+            .Generate();
+    }
+
+    [Fact]
+    public async Task ShouldNot_Login_UserDoesNotExists()
+    {
+        Email email;
+
+        // Arrange
+        var query = GetSutQuery();
+
+        email = Email.Create(query.Email).Value;
+
+        _authRepository.GetAuthByEmail(email, default)
+            .Returns(null as Auth);
+
+        // Act
+        var result = await _sut.Handle(query, default);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.Equal(result.FirstError.Code, Errors.User.EmailDoesNotExists.Code);
+    }
+
+    [Fact]
+    public async Task ShouldNot_Login_ProfileDoesNotExists()
+    {
+        // Arrange
+        var query = GetSutQuery();
+        Auth auth = GetAuth(query.Email);
+
+        _authRepository.GetAuthByEmail(auth.Email, default)
+           .Returns(auth);
+
+        _profileRepository.GetProfileByAuthId(
+            Arg.Any<AuthId>(), default)
+            .Returns(null as Profile);
+
+        // Act
+        var result = await _sut.Handle(query, default);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.Equal(result.FirstError.Code, Errors.User.ProfileDoesNotExists.Code);
+    }
+
+    [Fact]
+    public async Task ShouldNot_Login_IncorrectCredentials()
+    {
+        // Arrange
+        var query = GetSutQuery();
+        Auth auth = GetAuth(query.Email);
+
+        _authRepository.GetAuthByEmail(auth.Email, default)
+           .Returns(auth);
+
+        _profileRepository.GetProfileByAuthId(
+            auth.Id, default)
+            .Returns(GetProfile(auth.Id));
+
+        _passwordProvider.VerifyPassword(
+            query.Password, auth.Password)
+            .Returns(false);
+
+        // Act
+        var result = await _sut.Handle(query, default);
+
+        // Assert
+        Assert.True(result.IsError);
+        Assert.Equal(result.FirstError.Code, Errors.User.BadCredentials.Code);
+    }
+
+    [Fact]
+    public async Task Should_GenerateTokens_WhenLoginCorrectly()
+    {
+        // Arrange
+        var query = GetSutQuery();
+        Auth auth = GetAuth(query.Email);
+
+        _authRepository.GetAuthByEmail(auth.Email, default)
+           .Returns(auth);
+
+        _profileRepository.GetProfileByAuthId(
+            auth.Id, default)
+            .Returns(GetProfile(auth.Id));
+
+        _passwordProvider.VerifyPassword(
+            query.Password, auth.Password)
+            .Returns(true);
+
+        // Act
+        var result = await _sut.Handle(query, default);
+
+        // Assert
+        Assert.False(result.IsError);
+        Assert.NotNull(result.Value.AccessToken);
+        Assert.NotNull(result.Value.RefreshToken);
+    }
+}
