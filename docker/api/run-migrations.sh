3b4e514815d48200b58a531e95b5297b2431a0a9
Diogo Assunção <dassuncao1234@gmail.com>
2023-11-16 15:45:12 +0000

    add: XmlTypeConfiguration

--- docker/api/run-migrations.sh
+++ docker/api/run-migrations.sh
@@ -1,6 +1,3 @@
-dotnet tool install --global dotnet-ef --version 7.0.11
-PATH="${PATH}:/root/.dotnet/tools"
-
 dotnet ef database update InitialMigration --startup-project ../src/BeatEcoprove.Api/ --project ../src/BeatEcoprove.Infrastructure/
 
 dotnet BeatEcoprove.Api.dll
