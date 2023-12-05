2e1ee68ab6ecea2f048f6ab1ce332a56e44089f9
Diogo Assunção <dassuncao1234@gmail.com>
2023-12-05 00:42:33 +0000

    add: MaintenanceActivity and MaintenanceService Entities

---
+++ src/BeatEcoprove.Domain/ClosetAggregator/Entities/MaintenanceActivity.cs
@@ -0,0 +1,49 @@
+﻿using BeatEcoprove.Domain.ClosetAggregator.ValueObjects;
+using BeatEcoprove.Domain.ProfileAggregator.ValueObjects;
+
+namespace BeatEcoprove.Domain.ClosetAggregator.Entities;
+
+public class MaintenanceActivity : Activity
+{
+    private readonly List<MaintenanceService> _maintenanceServices = new();
+    
+    private MaintenanceActivity() { }
+    
+    private MaintenanceActivity(
+        ProfileId profileId, 
+        ClothId clothId,
+        string title,
+        string badge,
+        int pointsOfSustentability,
+        float xp = 0, 
+        float deltaScore = 0) : base(profileId, clothId, xp, deltaScore)
+    {
+        Title = title;
+        Badge = badge;
+        PointsOfSustentability = pointsOfSustentability;
+    }
+
+    public string Title { get; private set; }
+    public string Badge { get; private set; }
+    public int PointsOfSustentability { get; private set; }
+    public IReadOnlyList<MaintenanceService> MaintenanceServices => _maintenanceServices;
+    
+    public static MaintenanceActivity Create(
+        ProfileId profileId, 
+        ClothId clothId,
+        string title,
+        string badge,
+        int pointsOfSustentability,
+        float xp = 0, 
+        float deltaScore = 0)
+    {
+        return new MaintenanceActivity(
+            profileId, 
+            clothId, 
+            title,
+            badge,
+            pointsOfSustentability,
+            xp, 
+            deltaScore);
+    }
+}
---
+++ src/BeatEcoprove.Domain/ClosetAggregator/ValueObjects/MaintenanceServiceId.cs
@@ -0,0 +1,19 @@
+﻿using BeatEcoprove.Domain.Shared.Models;
+
+namespace BeatEcoprove.Domain.ClosetAggregator.ValueObjects;
+
+public class MaintenanceServiceId : ValueObject
+{
+    private MaintenanceServiceId() { }
+    private MaintenanceServiceId(Guid id) => Value = id;
+
+    public Guid Value { get; private set; }
+
+    public static MaintenanceServiceId CreateUnique() => new(Guid.NewGuid());
+
+    public static MaintenanceServiceId Create(Guid id) => new(id);
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
